63f375ae9c6eac7c817e3f9ab5229c94d0271208
shshsjjd <saad_sadness@mail.ru>
2024-12-13 09:50:52 +0300

    first

--- composeApp/src/iosMain/kotlin/com/example/sobes/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/example/sobes/MainViewController.kt
@@ -1,5 +1,11 @@
 package com.example.sobes
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.example.sobes.di.KoinInjector
+import com.example.sobes.screen.root.Root
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    KoinInjector.koinApp
+    Root()
+}
+
